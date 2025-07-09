6dae8f13b7e358bea61de32bb340a1ae8c577432
aggarwal07 <shivamaggarwal179@gmail.com>
2025-07-09 20:09:58 +0530

    footer complete

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -97,6 +97,61 @@ const HomePage = () => {
         </div>
       </div>
       {/* section 3 ends */}
+
+      {/* section 4 starts */}
+      <div className="mt-24 flex justify-between w-full">
+        <div className="w-[60%] font-semibold h-[55vh] flex flex-col justify-between">
+          <ul className="flex list-none gap-10">
+            <li>Events</li>
+            <li>Gen Ai</li>
+            <li>Careers</li>
+            <li>Case Study</li>
+            <li>SME Talks</li>
+          </ul>
+          <div className="text-[28px] w-[50%]">
+            <p>
+              For any collaborative projects or enquiries feel free to connect
+              with us.
+            </p>
+          </div>
+          <ul className="flex list-none gap-10 text-[14px] font-medium">
+            <li>About</li>
+            <li>Insights</li>
+            <li>Community</li>
+            <li>Privacy & Policies</li>
+          </ul>
+        </div>
+        <div className="w-[30%] flex flex-col items-end">
+          <p className="font-medium text-[75px] text-right fontPlayfair">
+            Connect <br /> With Us
+          </p>
+          <form className="flex flex-col gap-6 w-full mt-12">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="w-full border-b-2 border-[#FFFFFF33] bg-transparent focus:outline-none focus:border-cyan-500 py-2 text-sm"
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full border-b-2 border-[#FFFFFF33] bg-transparent focus:outline-none focus:border-cyan-500 py-2 text-sm"
+            />
+            <input
+              type="tel"
+              placeholder="Phone Number"
+              className="w-full border-b-2 border-[#FFFFFF33] bg-transparent focus:outline-none focus:border-cyan-500 py-2 text-sm"
+            />
+            <button
+              type="submit"
+              className="mt-4 border border-[#08A7CE] bg-[#08A7CE66] font-semibold px-4 py-2 rounded-full item-end w-36"
+            >
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </div>
+      {/* section 4 ends */}
     </div>
   );
 };
